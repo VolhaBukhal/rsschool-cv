5f67d8ede077b266dd5daa26922283c18cf520c8
ola <o.bukhal@gmail.com>
2021-09-19 16:47:12 +0300

    feat: add highlight and work section

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const burgerEl = document.querySelector('.burger');
 const menuLinks = document.querySelectorAll('.header__list-item_link');
 const nuvigationEl = document.querySelector('.header__nav');
+const bodyEl = document.querySelector('body');
+const workItems = document.querySelectorAll('.experience-item');
 
 
 burgerEl.addEventListener('click', () => {
     console.log('click');
     burgerEl.classList.toggle('active');
-    document.querySelector('body').classList.toggle('lock');
+    bodyEl.classList.toggle('lock');
     nuvigationEl.classList.toggle('active');
 
 })
@@ -19,3 +21,26 @@ menuLinks.forEach(link => link.addEventListener('click', () => {
         burgerEl.classList.remove('active');
     }
 }) );
+
+
+//popup the code example after click
+const codeEl = document.querySelector('.experience-work__row').firstElementChild;
+const modalEl = document.querySelector('.modal');
+codeEl.addEventListener('click', ()=> {
+    modalEl.classList.add('active');
+    bodyEl.classList.add('lock');
+});
+
+// open link in new window
+
+workItems.forEach(item => item.addEventListener('click', () => {
+    console.log('link clicked');
+    let linkHref = item.querySelector('.experience-item__link').href;
+    console.log(linkHref);
+    if(linkHref) {
+        window.open(linkHref);
+    } else {
+        return false;
+    }
+
+}))
